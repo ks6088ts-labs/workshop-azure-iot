a1ca88dae21b203334a2865fc4a11fca386e32c3
ks6088ts <ks6088ts@gmail.com>
2024-10-17 19:13:22 +0900

    remove default settings

--- workshop_azure_iot/settings/core.py
+++ workshop_azure_iot/settings/core.py
@@ -2,5 +2,5 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class CoreSettings(BaseSettings):
-    core_version: str = "0.0.0"
+    core_version: str
     model_config = SettingsConfigDict(env_file="core.env")
