3dad61f9c28249e07c0bad57924f8386ad7b87d1
ks6088ts <ks6088ts@gmail.com>
2024-10-19 08:19:19 +0900

    call direct method

--- workshop_azure_iot/settings/iot_hub.py
+++ workshop_azure_iot/settings/iot_hub.py
@@ -3,4 +3,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     iot_hub_device_connection_string: str
+    iot_hub_connection_string: str
+    iot_hub_device_id: str
     model_config = SettingsConfigDict(env_file="iot_hub.env")
